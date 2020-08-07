e7c3f9444d0712dad4784dc3c5c6fd980e2aad25
mdsaddamvol <mdsaddamvoltas@gmail.com>
2020-08-07 19:09:33 +0600

    responsiveness on carousel

--- src/components/sections/best-courses-section/courses-section.component.jsx
+++ src/components/sections/best-courses-section/courses-section.component.jsx
@@ -22,7 +22,21 @@ const Courses = ({ id, catagory }) => {
 		autoplayHoverPause: true,
 		rewind: true,
 		autoplay: true,
+		responsive: {
+			0: {
+				items: 1,
+				nav: true,
+			},
+			600: {
+				items: 3,
+				nav: false,
+			},
+			1000: {
+				items: 4,
+			},
+		},
 	};
+
 	return (
 		<div className='container'>
 			<div className='border-bottom '>
